34dbb41d794b327a094f6c24c00a303aba05f428
Yuna <yunafran@gmail.com>
2022-03-30 11:31:00 +0900

    feat: change setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fizzbuzz',
-    version="0.0.1",
+    version="0.0.2",
     description='Fizz Buzz program.',
     author='yufranc',
     url='https://github.com/yufranc/python-dev-tutorial',
@@ -10,6 +10,9 @@ setup(
     license='MIT',
     install_requires=[],
     packages=find_packages(exclude=["tests"]),
+    entry_points={
+        'console_scripts': 'fizzbuzz-cli = fizzbuzz.fizzbuzz:main'
+    },
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License"
